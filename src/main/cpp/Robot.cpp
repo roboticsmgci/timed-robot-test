90c05e84b5a6b41608d92abc53116816491c5c10
Steven Wu <93849557+wusteven815@users.noreply.github.com>
2022-03-02 13:52:33 -0500

    Add throttle level support

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -79,6 +79,8 @@ class Robot : public frc::TimedRobot {
   void TeleopPeriodic() {
     // Drive with arcade style
     m_robotDrive.ArcadeDrive(-m_stick.GetY(), m_stick.GetX());
+    // Test this
+    m_robotDrive.ArcadeDrive(-m_stick.GetY()*m_stick.GetThrottle(), m_stick.GetX()*m_stick.GetThrottle());
   }
 };
 
